42489c09ac7b619d63475be180bd402001d4e1ca
EdwardChung <leonardo071123@gmail.com>
2026-05-21 22:07:06 +0800

    課堂練習

--- src/junior/WhileLoopInfo.java
+++ src/junior/WhileLoopInfo.java
@@ -39,40 +39,64 @@ public class WhileLoopInfo {
         // System.out.println("while 迴圈結束");
 
         // 設定結束的起始條件: true:真的結束, false:還沒結束
-        boolean end = false;
+        // boolean end = false;
 
-        Scanner sc = new Scanner(System.in);
-        // 總存款金額
-        int totalMoney = 0;
-        while (end == false) {
-            System.out.println("請輸入要辦理項目,1:提領,2:存款,3:餘額查詢,4:結束");
-            int getInputNum = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // // 總存款金額
+        // int totalMoney = 0;
+        // while (end == false) {
+        // System.out.println("請輸入要辦理項目,1:提領,2:存款,3:餘額查詢,4:結束");
+        // int getInputNum = sc.nextInt();
 
-            if (getInputNum == 1) {
-                // 防呆 總存款金額 要大於等於 提領金額
-                if (totalMoney >= 1000) {
-                    totalMoney -= 1000;
-                    System.out.println("您已提領1000元成功");
-                } else {
-                    System.out.println("您的餘額不足");
-                }
-            } else if (getInputNum == 2) {
-                totalMoney += 1000;
-                System.out.println("您已存款1000元成功");
-            } else if (getInputNum == 3) {
-                System.out.println("您的餘額為" + totalMoney + "元");
-            } else if (getInputNum == 4) {
-                System.out.println("謝謝您的使用");
-                end = true;
-            } else {
-                System.out.println("您輸入的數字錯誤,請重新輸入");
-            }
+        // if (getInputNum == 1) {
+        // // 防呆 總存款金額 要大於等於 提領金額
+        // if (totalMoney >= 1000) {
+        // totalMoney -= 1000;
+        // System.out.println("您已提領1000元成功");
+        // } else {
+        // System.out.println("您的餘額不足");
+        // }
+        // } else if (getInputNum == 2) {
+        // totalMoney += 1000;
+        // System.out.println("您已存款1000元成功");
+        // } else if (getInputNum == 3) {
+        // System.out.println("您的餘額為" + totalMoney + "元");
+        // } else if (getInputNum == 4) {
+        // System.out.println("謝謝您的使用");
+        // end = true;
+        // } else {
+        // System.out.println("您輸入的數字錯誤,請重新輸入");
+        // }
 
-            System.out.println("這回合已經結束");
-        }
+        // System.out.println("這回合已經結束");
+        // }
 
-        sc.close();
+        // sc.close();
 
         // 課堂練習 用while 迴圈 讓使用者 輸入七個不重複的數 (1 ~ 100) 存到陣列 再用for 迴圈印出這七個數字
+        Scanner sc2 = new Scanner(System.in);
+        // while的起始條件
+        int count = 0;
+
+        int[] numbers = new int[7];
+
+        while (count < 7) {
+            System.out.println("請輸入第" + (count + 1) + "個數字");
+            int getInputNum = sc2.nextInt();
+            // 防呆 檢查一：檢查數字有沒有在 1 ~ 100 的範圍內
+            if (getInputNum >= 1 && getInputNum <= 100) {
+                System.out.println("您輸入的數字為:" + getInputNum);
+                numbers[count] = getInputNum;
+                count++;
+            } else {
+                System.out.println("您輸入的數字不在1 ~ 100 的範圍內,請重新輸入");
+            }
+
+        }
+
+        for (int z = 0; z < numbers.length; z++) {
+            System.out.print(numbers[z] + ",");
+        }
+        sc2.close();
     }
 }
