670197fbd0dcb7b064a8ed2f952f9bea771e58fe
EdwardChung <leonardo071123@gmail.com>
2026-05-28 20:24:45 +0800

    作業解答

--- src/junior/MethodInfo.java
+++ src/junior/MethodInfo.java
@@ -2,11 +2,15 @@ package junior;
 
 public class MethodInfo {
     public static void main(String[] args) {
-        printHello1();
+
+        for (int i = 0; i < 5; i++) {
+            printHello1();
+        }
 
         printHello2(4);
         printHello2(5);
         printHello2(6);
+        printHello2(20);
 
         printHello3("Edward");
         printHello3("David");
@@ -14,7 +18,7 @@ public class MethodInfo {
         printHello4("Allen", 7);
         printHello4("Ben", 8);
 
-        String[] names = { "Edward1", "David1", "Allen1" };
+        String[] names = { "Edward1", "David1", "Allen1", "Ben1" };
         // for(int z = 0; z < names.length; z++) {
         // System.out.println("Hello, " + names[z]);
         // }
@@ -31,6 +35,7 @@ public class MethodInfo {
 
         String strResult = stringAdd("Hello1, ", "World1!");
         System.out.println(strResult);
+
     }
 
     // 方法(Method)的定義 其他語言或許是function
@@ -67,8 +72,15 @@ public class MethodInfo {
         }
     }
 
+    // 理想長度的陣列:兩個長度都相等
+    // String[] names = {"Edward", "David", "Allen", "Ben", "Cathy"};
+    // int[] nums = {1, 2, 3, 4, 5};
+
     // String[] names = {"Edward", "David", "Allen"};
     // int[] nums = {1, 2, 3, 4, 5};
+
+    // String[] names = {"Edward", "David", "Allen", "Ben", "Cathy"};
+    // int[] nums = {1, 2, 3};
     // 如何印出以下結果:
     // Hello, Edward, 1!
     // Hello, David, 2!
@@ -77,9 +89,35 @@ public class MethodInfo {
     // Hello, empty, 5!
 
     public static void printHello6(String[] names, int[] nums) {
-        for (int i = 0; i < names.length; i++) {
-            System.out.println("Hello, " + names[i] + "!");
+        // 取得陣列的長度
+        int namesLength = names.length;
+        int numsLength = nums.length;
+
+        // 如果names的長度比較大 就以names的長度為主
+        if (namesLength > numsLength) {
+            for (int i = 0; i < namesLength; i++) {
+                // 預期印出, 但是因為nums的長度比較短, 所以系統會彈出例外的錯誤並且終止程式
+                // System.out.println("Hello, " + names[i] + ", " + nums[i] + "!");
+
+                // i 就要判斷超過nums的長度了嗎? 超過了就印出0, 沒有超過就預期印出
+                if (i < numsLength) {
+                    System.out.println("Hello, " + names[i] + ", " + nums[i] + "!");
+                } else {
+                    System.out.println("Hello, " + names[i] + ", 0!");
+                }
+
+            }
+        } else { // 如果nums的長度比較大 就以nums的長度為主
+            for (int i = 0; i < numsLength; i++) {
+                // i 就要判斷超過names的長度了嗎? 超過了就印出empty, 沒有超過就預期印出
+                if (i < namesLength) {
+                    System.out.println("Hello, " + names[i] + ", " + nums[i] + "!");
+                } else {
+                    System.out.println("Hello, empty, " + nums[i] + "!");
+                }
+            }
         }
+
     }
 
     public static int add(int num1, int num2) {
