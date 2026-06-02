e13076a113a802cdc43baeea2eda26d6b9987129
EdwardChung <leonardo071123@gmail.com>
2026-06-02 19:58:30 +0800

    覆寫1

--- src/junior/inheritanceIntroduction/Cat.java
+++ src/junior/inheritanceIntroduction/Cat.java
@@ -4,7 +4,6 @@ public class Cat extends Animal {
 
     @Override
     public void eat() {
-        super.eat(); // 呼叫父類別的 eat 方法
-        // System.out.println("Cat1 is eating.");
+        System.out.println("Cat1 is eating.");
     }
 }
