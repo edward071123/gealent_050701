056de0adbd2926362cc36aa32bb145356d0a05dd
edwardMacAir2026 <leonardo071123@gmail.com>
2026-06-09 18:50:06 +0800

    課後練習解題

--- src/junior/polymorphism/Demo.java
+++ src/junior/polymorphism/Demo.java
@@ -40,16 +40,6 @@ public class Demo {
         for (int i = 0; i < employees.length; i++) {
             employees[i].calculateSalary();
         }
-        // ------------------ 使用多型 + array + for 處理 ------------------
-
-        // 多型課後練習:
-        // 父類別: Vehicle(車輛)
-        // 有一個方法 move()
-        // 以下子類別請覆寫 父類別的move()
-        // 子類別1: Car(汽車) , 覆寫印出: 汽車在道路行駛
-        // 子類別2: Bike(腳踏車) , 覆寫印出: 腳踏車在自行車道騎乘
-        // 子類別3: Bus(公車) , 覆寫印出: 公車載客前行
-        // 請用多型 + array + for 印出20個車輛的 行駛方式
 
     }
 }
