895115b1913f71f27a4f7c527c2a57fd1af674cc
EdwardChung <leonardo071123@gmail.com>
2026-06-11 20:39:26 +0800

    抽象與類別

--- src/junior/abstractAndInterface1/Demo.java
+++ src/junior/abstractAndInterface1/Demo.java
@@ -3,6 +3,7 @@ package junior.abstractAndInterface1;
 public class Demo {
     public static void main(String[] args) {
         // 抽象 + 介面的混用
+
         Iphone i14 = new Iphone("iPhone 14");
         i14.powerOn();
         i14.display();
---
+++ src/junior/abstractAndInterface2/Demo.java
@@ -0,0 +1,5 @@
+package junior.abstractAndInterface2;
+
+public class Demo {
+
+}
