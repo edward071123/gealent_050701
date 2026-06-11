af3215b6950fed7201d99420ad96bac61f578523
EdwardChung <leonardo071123@gmail.com>
2026-06-11 21:35:35 +0800

    抽象mix

--- src/junior/abstractAndInterface2/Demo.java
+++ src/junior/abstractAndInterface2/Demo.java
@@ -1,15 +1,23 @@
 package junior.abstractAndInterface2;
 
+import junior.abstractAndInterface2.abstractExample.ACreditCardPaymentService;
 import junior.abstractAndInterface2.abstractExample.ALinePayPaymentService;
 import junior.abstractAndInterface2.abstractExample.AOrderService;
 
 public class Demo {
     public static void main(String[] args) {
-        ALinePayPaymentService linePayPayment = new ALinePayPaymentService();
 
+        // 使用LinePay付款(ALinePayPaymentService)
+        ALinePayPaymentService linePayPayment = new ALinePayPaymentService();
         // new 就是類別(class)已經被生產出來 變成物件(object)
         AOrderService order1 = new AOrderService(linePayPayment);
-        order1.createOrder(1000);
+        order1.createOrderLinePay(1000);
+
+        System.out.println("=================================================");
+        // 課堂練習 AOrderService order2 使用信用卡付款(ACreditCardPaymentService)
+        ACreditCardPaymentService creditCardPayment = new ACreditCardPaymentService();
+        AOrderService order2 = new AOrderService(creditCardPayment);
+        order2.createOrderCreditCard(2000);
 
     }
 }
--- src/junior/abstractAndInterface2/abstractExample/AOrderService.java
+++ src/junior/abstractAndInterface2/abstractExample/AOrderService.java
@@ -2,6 +2,7 @@ package junior.abstractAndInterface2.abstractExample;
 
 public class AOrderService {
     private ALinePayPaymentService linePayPaymentService;
+    private ACreditCardPaymentService creditCardPaymentService;
 
     // 建構子注入傳入的物件, 初始化的時候就可以使用LinePay付款
     // 類別去使用另外一個物件(物件注入)
@@ -10,9 +11,19 @@ public class AOrderService {
         // this.linePayPaymentService = new ALinePayPaymentService();
     }
 
-    public void createOrder(int amount) {
-        System.out.println("建立訂單");
+    public AOrderService(ACreditCardPaymentService creditCardPaymentService) {
+        this.creditCardPaymentService = creditCardPaymentService;
+        // this.creditCardPaymentService = new ACreditCardPaymentService();
+    }
+
+    public void createOrderLinePay(int amount) {
+        System.out.println("建立訂單(LinePay)");
         this.linePayPaymentService.pay(amount);
+    }
+
+    public void createOrderCreditCard(int amount) {
+        System.out.println("建立訂單(CreditCard)");
+        this.creditCardPaymentService.pay(amount);
     }
 
 }
