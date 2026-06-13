8e3b60a0f0ff292cd163d626ab73284641dcc149
EdwardChung <leonardo071123@gmail.com>
2026-06-13 11:01:44 +0800

    interface 付款方式與抽象類別 付款方式比對

--- src/junior/abstractAndInterface2/Demo.java
+++ src/junior/abstractAndInterface2/Demo.java
@@ -3,48 +3,51 @@ package junior.abstractAndInterface2;
 import junior.abstractAndInterface2.abstractExample.ACreditCard;
 import junior.abstractAndInterface2.abstractExample.ALinePay;
 import junior.abstractAndInterface2.abstractExample.AOrder;
+import junior.abstractAndInterface2.interfaceExample.ICreditCard;
+import junior.abstractAndInterface2.interfaceExample.ILinePay;
+import junior.abstractAndInterface2.interfaceExample.IOrder;
 
 public class Demo {
     public static void main(String[] args) {
 
-        /* 課後練習: 請練習此範例至少三次
+        /*
+         * 課後練習: 請練習此範例至少三次
          * 1. 先建立抽象類別:APayment
-         *    兩個屬性: paymaneName(付款名稱), feeRate(手續費率)
+         * 兩個屬性: paymaneName(付款名稱), feeRate(手續費率)
          * 
-         *    建構子: public APayment(String paymaneName, double feeRate)
+         * 建構子: public APayment(String paymaneName, double feeRate)
          * 
-         *    共同方法: public void pay(int amount)
+         * 共同方法: public void pay(int amount)
          * 
-         *    抽象方法: public abstract void processPayment(int amount, int fee)
+         * 抽象方法: public abstract void processPayment(int amount, int fee)
          *
-         *    私有方法: private String validateAmount(int amount)
-         *             private int calculateFee(int amount)
-         *             private void printPaymentResult(int amount, double fee)
+         * 私有方法: private String validateAmount(int amount)
+         * private int calculateFee(int amount)
+         * private void printPaymentResult(int amount, double fee)
          * 
-         * 2. 建立付款方式的一般類別且都要繼承APayment: 
-         *    信用卡: ACreditCard
-         *    LinePay: ALinePay
-         *    
-         *    需要建構子: 記得內容也要用super
-         *    
-         *    需要實做出繼承的抽象方法: public void processPayment(int amount, int fee)
+         * 2. 建立付款方式的一般類別且都要繼承APayment:
+         * 信用卡: ACreditCard
+         * LinePay: ALinePay
+         * 
+         * 需要建構子: 記得內容也要用super
+         * 
+         * 需要實做出繼承的抽象方法: public void processPayment(int amount, int fee)
          *
          * 3. 建立一般類別AOrder
-         *    建構子: 付款的物件傳入(物件注入)
-         *    
-         *    建立公開發方法: createOrderLinePay(int amount)
-         *                   createOrderCreditCard(int amount)
+         * 建構子: 付款的物件傳入(物件注入)
          * 
-         *    公開方法的內容為: 印出"建立訂單(付款名稱)"
-         *                   呼叫付款物件的pay方法
+         * 建立公開發方法: createOrderLinePay(int amount)
+         * createOrderCreditCard(int amount)
+         * 
+         * 公開方法的內容為: 印出"建立訂單(付款名稱)"
+         * 呼叫付款物件的pay方法
          * 
          * 4. 在程式進入點內 實體化AOrder 類別且呼叫 createOrderLinePay or createOrderCreditCard 方法
-         *  
+         * 
          */
 
         // 範例互動網站:
         // https://edward071123.github.io/gealent_050701/src/junior/abstractAndInterface2/object-call-visualizer.html
-
 
         // 使用LinePay付款(ALinePay) 物件(object)先比喻成模組(module)
         ALinePay linePayPayment = new ALinePay();
@@ -57,6 +60,20 @@ public class Demo {
         ACreditCard creditCardPayment = new ACreditCard();
         AOrder order2 = new AOrder(creditCardPayment);
         order2.createOrderCreditCard(2000);
+
+        System.out.println("=================================================");
+        AOrder order3 = new AOrder(creditCardPayment);
+        order3.createOrderCreditCard(-2000);
+
+        System.out.println("====================以下為interface範例=============================");
+        ILinePay iLinePay = new ILinePay();
+        IOrder order4 = new IOrder(iLinePay);
+        order4.createOrderLinePay(1000);
+
+        System.out.println("=================================================");
+        ICreditCard iCreidCard = new ICreditCard();
+        IOrder order5 = new IOrder(iCreidCard);
+        order5.createOrderCreditCard(3000);
 
     }
 }
