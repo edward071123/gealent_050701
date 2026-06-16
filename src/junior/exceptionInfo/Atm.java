ddfdf5cf0f5781b92498a252a54327d3f8ebf9bd
EdwardChung <leonardo071123@gmail.com>
2026-06-16 19:16:49 +0800

    try catch

--- src/junior/exceptionInfo/Atm.java
+++ src/junior/exceptionInfo/Atm.java
@@ -20,7 +20,8 @@ public class Atm {
         System.out.println("可以提領");
     }
 
-    /* 提款(throws 與 throw)
+    /*
+     * 提款(throws 與 throw)
      * throws Exception 表示：
      * 這個方法可能發生例外，但不在方法內使用 try-catch 處理，
      * 而是把例外交給呼叫這個方法的人處理。
@@ -28,14 +29,14 @@ public class Atm {
     public void withdraw(int amount, boolean exceptions) throws Exception {
         int balance = 100;
         if (amount <= 0) {
-            throw new Exception("金額不能小於0");
+            throw new Exception("進階2-金額不能小於0");
         }
 
         if (amount > balance) {
-            throw new Exception("餘額不足");
+            throw new Exception("進階2-餘額不足");
         }
 
-        System.out.println("可以提領");
+        System.out.println("進階2-可以提領");
     }
 
 }
--- src/junior/exceptionInfo/Demo.java
+++ src/junior/exceptionInfo/Demo.java
@@ -12,7 +12,7 @@ public class Demo {
 
             // 程式執行內容
             int a1 = 10;
-            int b1 = 10;            // 改0會出錯且跳到catch
+            int b1 = 10; // 改0會出錯且跳到catch
             int result1 = a1 / b1;
             System.out.println(result1);
 
@@ -24,7 +24,7 @@ public class Demo {
             int[] a3 = { 1, 2, 3, 4, 5 };
             System.out.println(a3[10]); // 改a3[10]會出錯且跳到catch
 
-            int guess = sc4.nextInt();   // 應該要輸入數字的 嘗試輸入"aaa"
+            int guess = sc4.nextInt(); // 應該要輸入數字的 嘗試輸入"aaa"
             System.out.println("您輸入的數字為:" + guess);
 
         } catch (Exception e) {
@@ -35,18 +35,30 @@ public class Demo {
             sc4.close();
         }
 
-        System.out.println("========================進階=============================");
+        System.out.println("========================進階1=============================");
 
         // 呼叫範例
         try {
             Atm a1 = new Atm();
-            a1.withdraw(-1);          // 可以替換 200, 100, -1
+            a1.withdraw(-1); // 可以替換 200, 100, -1
         } catch (IllegalArgumentException e1) {
             System.out.println(e1.getMessage());
         } catch (ArithmeticException e2) {
             System.out.println(e2.getMessage());
         } catch (Exception e3) {
             System.out.println(e3.getMessage());
+        }
+
+        System.out.println("========================進階2=============================");
+
+        // 呼叫範例
+        try {
+            Atm a2 = new Atm();
+            // a2.withdraw(-1, true); // 可以替換 200, 100, -1
+            // a2.withdraw(200, true);
+            a2.withdraw(100, true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
     }
