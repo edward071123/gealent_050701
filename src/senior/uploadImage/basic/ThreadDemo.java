a4b882b5c163236312d83b18e27f97d7f3d0f159
EdwardChung <leonardo071123@gmail.com>
2026-06-23 21:03:15 +0800

    整理筆記

--- src/senior/uploadImage/basic/ThreadDemo.java
+++ src/senior/uploadImage/basic/ThreadDemo.java
@@ -3,6 +3,7 @@ package senior.uploadImage.basic;
 public class ThreadDemo {
     public static void main(String[] args) {
         // 執行緒介紹
+        // 情景: 生產線, 搶票, 影音串流, 上傳圖片, 多個重複的事情....
 
         // |---------|---------主執行緒(main)-----------------------
         /////////////|
@@ -18,12 +19,12 @@ public class ThreadDemo {
         //             try {
         //                 // 1000毫秒 = 1秒
         //                 // 睡一秒鐘
-        //                 Thread.sleep(1000);
+        //             Thread.sleep(1000);
         //             } catch (Exception e) {
-        //                 e.printStackTrace();
-        //             }
+        //             e.printStackTrace();
         //         }
         //     }
+        // }
         // });
 
         // Thread thread2 = new Thread(() -> {
@@ -31,11 +32,11 @@ public class ThreadDemo {
         //         System.out.println("執行緒 2 正在執行: " + i);
         //         try {
         //             // 睡兩秒鐘
-        //             Thread.sleep(2000);
+        //         Thread.sleep(2000);
         //         } catch (Exception e) {
-        //             e.printStackTrace();
-        //         }
+        //         e.printStackTrace();
         //     }
+        // }
         // });
 
         // // 排隊執行 thread1 全部跑完後 換 thread2
@@ -46,7 +47,7 @@ public class ThreadDemo {
         // thread1.start();
         // thread2.start();
 
-        //========================main 執行緒查看================================
+        // ========================main 執行緒查看================================
         System.out.println("main-thread start...");
 
         Thread thread3 = new Thread(() -> {
@@ -59,12 +60,12 @@ public class ThreadDemo {
             }
             System.out.println("thread3 end.");
         });
-        thread3.start();
-        
+
         try {
             Thread.sleep(1000);
-        } catch (InterruptedException e) {}
-
+        } catch(InterruptedException e) {}
+        
+        
         System.out.println("main-thread end...");
 
     }
